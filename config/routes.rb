404db554f90986d4f331c342e55fd90d33137440
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-25 09:38:01 +0000

    [Finish]9b日付検索

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    get 'search' => 'users#search'
+  end
   resources :books
 end
